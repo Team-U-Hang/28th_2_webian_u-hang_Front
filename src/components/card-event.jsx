50527ba4c4c4f6bf0f745e4056954ed6ef990e0d
jiyoon <starbuun21@sookmyung.ac.kr>
2024-01-26 13:31:45 +0900

    Feat: 메인 페이지

---
+++ src/components/card-event.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components"
+import sookmyung from '../assets/sookmyung.jpg'
+import COLOR from "../utils/color";
+
+const Box = styled.div`
+    background-color: white;
+    border: 1px solid gray;
+    border-radius: 10%;
+    box-shadow: 3px 5px 10px gray;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 2%;
+    cursor: pointer;
+    &:hover{
+        transform: scale(1.05);
+    }
+    img{
+        width: 100%;
+        height: 40%;
+    }
+    #text1{
+        font-size: 15px;
+        font-weight: 600;
+        color: ${COLOR.green};
+    }
+    #title{
+        font-size: 20px;
+        font-weight: 600;
+    }
+    #text2{
+        font-size: 13px;
+    }
+`;
+
+export default function CardEvent(props){
+    return(
+        <Box>
+            <img src={sookmyung}/>
+            <text id="text1">{props.group}</text>
+            <text id="title">{props.title}</text>
+            <text id="text2">신청: {props.apply}</text>
+            <text id="text2">운영: {props.period}</text>
+        </Box>
+    )
+}
